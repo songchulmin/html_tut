cc0907addbfb727423946636e7500755a5af7032
songchulmin <rednadasea@gmail.com>
2017-08-09 10:58:08 +0900

    dd

---
+++ 05_js_ex_05.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017-06-28.
+ */
+
+var kkk = 90;
+var eee = 80;
+
+if ( (kkk >= 90) && (eee >= 90) ){
+
+  console.log('수');
+
+} else if ( (kkk >= 90) && (eee >= 80) ){
+
+  console.log('우');
+
+} else if ( (kkk >= 80) && (eee >= 80) ){
+
+  console.log('미');
+
+} else {
+
+  console.log('낙제');
+}
+
